1af1c6b9dc29ed12470823da307e1d00f2e6a457
bill broadley <bill@broadle.org>
2019-11-28 05:15:45 -0800

    fixed runbench

--- runbench.sh
+++ runbench.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-rm etc/fs-meta.sql; go run walker.go --debug --new-db --threads=1 | grep threads > r
-rm etc/fs-meta.sql; go run walker.go --debug --new-db --threads=2 | grep threads >> r
-rm etc/fs-meta.sql; go run walker.go --debug --new-db --threads=4 | grep threads >> r
-rm etc/fs-meta.sql; go run walker.go --debug --new-db --threads=8 | grep threads >> r
+rm etc/fs-meta.sql; go run walker.go --debug --new-db --threads=1 &> log; cat log | grep threads > r
+rm etc/fs-meta.sql; go run walker.go --debug --new-db --threads=2 &> log; cat log | grep threads >> r
+rm etc/fs-meta.sql; go run walker.go --debug --new-db --threads=4 &> log; cat log | grep threads >> r
